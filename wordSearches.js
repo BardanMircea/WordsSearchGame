139762ec814a150c6d947bffcdb3094639a36c3b
mircea bardan <bardan.mircea.calin@gmail.com>
2023-04-06 21:52:06 +0200

    added solution to extract and traverse ALL the diagonals of the grid and search them for hidden words (in both directions)

--- wordSearches.js
+++ wordSearches.js
@@ -16,11 +16,27 @@ const wordSearches = () => {
 
 // first function called by wordSearches()
 function checkGrid(rowsGrid, hiddenWordsArr) {
-  // using the original grid, create a second grid that transforms the argument's columns into rows, so that we can easily traverse the columns
-  const columnsGrid = turnColumnsToRows(rowsGrid);
+  // console.log(rowsGrid)
 
-  // *INTERMEDIATE* extract the 2 diagonals, so we can check them agains the list of words
-  let diagonals = extractDiagonals(columnsGrid);
+  // using the original grid, create a second grid that transforms the argument's columns into rows, so that we can easily traverse the columns
+  let columnsGrid = turnColumnsToRows(rowsGrid);
+
+  // change the names from hiddenWords found in the grid to uppercase
+  // first: the names found inside columns
+  turnWordsToUpperCase(columnsGrid, hiddenWordsArr);
+  // console.log(columnsGrid)
+
+  // then rotate the upper-converted columns back into rows
+  rowsGrid = turnColumnsToRows(columnsGrid);
+  // console.log(rowsGrid)
+
+  // then convert the names inside the rows to uppercase => now we have all names from the hiddenNamesArr that are found in columns or rows converted to uppercase
+  turnWordsToUpperCase(rowsGrid, hiddenWordsArr);
+  // console.log(rowsGrid)
+
+  // *INTERMEDIATE* extract all the diagonals, so we can check them against the list of words
+  let diagonals = extractDiagonals(rowsGrid);
+  // console.log(diagonals)
 
   // traversing the rowsGrid line by line and searching for the hidden words inside each line, left-to-right and right-to-left
   // if any words are found, remove them from the hiddenWords
@@ -33,7 +49,7 @@ function checkGrid(rowsGrid, hiddenWordsArr) {
     checkLine(line, hiddenWordsArr);
   }
 
-  // same thing for the 2 main diagonals
+  // same thing for all the diagonals
   for (const diag of diagonals) {
     checkLine(diag, hiddenWordsArr);
   }
@@ -42,23 +58,88 @@ function checkGrid(rowsGrid, hiddenWordsArr) {
   console.log(hiddenWordsArr);
 }
 
-// function to extract the 2 main diagonals from grid (will offer solution to extract all the diagonals as a bonus, after the ADVANCED solution)
-function extractDiagonals(grid) {
-  let leftDiag = "";
-  let rightDiag = "";
-
+// function to turn words present both in the grid  and in the words array to upperCase
+function turnWordsToUpperCase(grid, wordsArr) {
   for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      if (i === j) {
-        leftDiag += grid[i][j];
+    for (let j = 0; j < wordsArr.length; j++) {
+      let reversed = grid[i].split("").reverse().join("");
+      if (grid[i].includes(wordsArr[j])) {
+        grid[i] = grid[i].replace(wordsArr[j], wordsArr[j].toUpperCase());
       }
-      if (i === grid[i].length - 1 - j && j === grid[i].length - 1 - i) {
-        rightDiag += grid[j][i];
+      if (reversed.includes(wordsArr[j])) {
+        grid[i] = grid[i].replace(
+          wordsArr[j].split().reverse().join(""),
+          wordsArr[j].split().reverse().join("").toUpperCase()
+        );
       }
     }
   }
+}
 
-  return [leftDiag, rightDiag];
+// function to extract all diagonals from grid
+function extractDiagonals(grid) {
+  let diags = [];
+
+  // etract upper left-side diagonals
+  for (let i = 0; i < grid.length; i++) {
+    let tempI = i;
+    let tempJ = 0;
+    let diag = "";
+    while (tempI >= 0 && tempJ < grid[i].length) {
+      diag += grid[tempI][tempJ];
+      tempI--;
+      tempJ++;
+    }
+    diags.push(diag);
+  }
+
+  // console.log(diags)
+
+  // extract lower right-side diagonals
+  for (let i = 0; i <= grid.length - 1; i++) {
+    let tempI = grid.length - 1;
+    let tempJ = i;
+    let diag = "";
+    while (tempI >= 0 && tempJ < grid[i].length) {
+      diag += grid[tempI][tempJ];
+      tempI--;
+      tempJ++;
+    }
+    diags.push(diag);
+  }
+  // console.log(diags)
+
+  // extract upper right-side diagonals
+  for (let i = 0; i < grid.length; i++) {
+    let tempI = 0;
+    let tempJ = grid.length - 1 - i;
+    let diag = "";
+    while (tempJ < grid.length) {
+      diag += grid[tempI][tempJ];
+      tempI++;
+      tempJ++;
+    }
+    diags.push(diag);
+  }
+
+  // console.log(diags)
+
+  // extract lower left-side diagonals
+  for (let i = 1; i < grid.length; i++) {
+    let tempI = i;
+    let tempJ = 0;
+    let diag = "";
+    while (tempI < grid.length) {
+      diag += grid[tempI][tempJ];
+      tempI++;
+      tempJ++;
+    }
+    diags.push(diag);
+  }
+
+  // console.log(diags)
+  // diags = diags.filter(elem => elem.length >= 4)
+  return diags;
 }
 
 // function to transform a grid by replacing its rows with its columns
@@ -75,19 +156,25 @@ function turnColumnsToRows(initialGrid) {
   return columnsGrid;
 }
 
-// function to check each line, and corresponding reversed line, for words that are inside the array passed as a param.
+// function to check each line, and corresponding reverse line, for words that are inside the array
 // Removes any words that are found inside the line from the array and returns the updated array
 function checkLine(line, hiddenWordsArr) {
-  const reverseLine = line.split("").reverse().join("");
+  let reverseLine = line.split("").reverse().join("");
   for (let i = 0; i < hiddenWordsArr.length; i++) {
     if (
-      line.includes(hiddenWordsArr[i]) ||
-      reverseLine.includes(hiddenWordsArr[i])
+      line.toLowerCase().includes(hiddenWordsArr[i]) ||
+      reverseLine.toLowerCase().includes(hiddenWordsArr[i])
     ) {
+      // if(reverseLine.toLowerCase().includes(hiddenWordsArr[i])){
+      //     reverseLine = reverseLine.replace(hiddenWordsArr[i], hiddenWordsArr[i].toUpperCase())
+      //     upperCaseNamesLines.push(reverseLine.split("").reverse().join(""))
+      // } else if(line.includes(hiddenWordsArr[i])){
+      //     line = line.replace(hiddenWordsArr[i], hiddenWordsArr[i].toUpperCase())
+      //     upperCaseNamesLines.push(line)
+      // }
       hiddenWordsArr.splice(i, 1);
     }
   }
-  return hiddenWordsArr;
 }
 
 // function that reads a file, writes its contents into the array and returns the array
